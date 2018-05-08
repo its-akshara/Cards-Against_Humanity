7aa0725308745c16d7705c1c2ef77eb79fa62e4e
Andrew Piro <andrewpiro@wifi-131-179-18-134.host.ucla.edu>
2018-05-08 13:43:33 -0700

    actually adding basic load screen and player screen

--- Frontend/cardsagainsthumanityfrontend/app/src/main/java/m117/andrewpiro/cardsagainsthumanityfrontend/StartActivity.java
+++ Frontend/cardsagainsthumanityfrontend/app/src/main/java/m117/andrewpiro/cardsagainsthumanityfrontend/StartActivity.java
@@ -1,14 +1,26 @@
 
 package m117.andrewpiro.cardsagainsthumanityfrontend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+    Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        startButton = (Button)findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), PlayerActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
