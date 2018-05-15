46751ab0a08fbf51d09e50c9d936535831bcaad9
Andrew Piro <andrewpiro@s-169-232-87-180.resnet.ucla.edu>
2018-05-15 13:49:34 -0700

    Updated player activity to work with random cards from card parser

--- Frontend/cardsagainsthumanityfrontend/app/src/main/java/m117/andrewpiro/cardsagainsthumanityfrontend/PlayerActivity.java
+++ Frontend/cardsagainsthumanityfrontend/app/src/main/java/m117/andrewpiro/cardsagainsthumanityfrontend/PlayerActivity.java
@@ -4,23 +4,65 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import java.util.Random;
+
+import cahCardParser.cardParser;
 
 public class PlayerActivity extends AppCompatActivity {
     TextView playerIDDisplay;
     TextView blackCardQuestion;
     static int count = 1;
-    final int CARD_NUMBER = 2;
+    final int CARD_NUMBER = 4;
     int selectedCard = -1;
     CharSequence Question = "What college do you go to?";
     TextView[] cards = new TextView[CARD_NUMBER];
     CharSequence[] cardText = new CharSequence[CARD_NUMBER];
+    cardParser cp = new cardParser();
+    int numBlackCards = cp.getNumberOfBlackCards();
+    int numWhiteCards = cp.getNumberOfWhiteCards();
+    Random rand = new Random();
+    Button update;
 
-
+    /*
     protected void hardCode()
     {
         cardText[0] = "USC";
         cardText[1] = "UCLA";
+    }
+    */
+
+    protected void update(){
+        //randomly select one black card
+        int bCardID = rand.nextInt(numBlackCards);
+        Question = cp.getBlackCardByIndex(bCardID);
+
+        //receive the info about the black card from a server. Use the global value
+        blackCardQuestion = (TextView) findViewById(R.id.blackCardQuestion);
+        blackCardQuestion.setText(Question);
+
+        cards[0] = (TextView) findViewById(R.id.card0);
+        cards[1] = (TextView) findViewById(R.id.card1);
+        cards[2] = (TextView) findViewById(R.id.card2);
+        cards[3] = (TextView) findViewById(R.id.card3);
+
+        //need to display cards you can choose
+        for(int i = 0; i<CARD_NUMBER; i++)
+        {
+            int wCardID = rand.nextInt(numWhiteCards);
+            cardText[i] = cp.getWhiteCardByIndex(wCardID);
+
+        }
+
+
+        //JSON parser stuff
+
+
+        cards[0].setText(cardText[0]);
+        cards[1].setText(cardText[1]);
+        cards[2].setText(cardText[2]);
+        cards[3].setText(cardText[3]);
     }
 
     @Override
@@ -28,34 +70,50 @@ public class PlayerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
-        hardCode();
+        //hardCode();
+
+
 
         playerIDDisplay= (TextView) findViewById(R.id.playerID);
         playerIDDisplay.setText("Player " + count);
 
+        //randomly select one black card
+        int bCardID = rand.nextInt(numBlackCards);
+        Question = cp.getBlackCardByIndex(bCardID);
+
         //receive the info about the black card from a server. Use the global value
         blackCardQuestion = (TextView) findViewById(R.id.blackCardQuestion);
         blackCardQuestion.setText(Question);
-        //need to display cards you can choose
-//        for(int i = 0; i<CARD_NUMBER; i++)
-//        {
-//            cards[i] = (TextView) findViewById();
-//        }
+
         cards[0] = (TextView) findViewById(R.id.card0);
         cards[1] = (TextView) findViewById(R.id.card1);
+        cards[2] = (TextView) findViewById(R.id.card2);
+        cards[3] = (TextView) findViewById(R.id.card3);
+
+        //need to display cards you can choose
+        for(int i = 0; i<CARD_NUMBER; i++)
+        {
+            int wCardID = rand.nextInt(numWhiteCards);
+            cardText[i] = cp.getWhiteCardByIndex(wCardID);
+
+        }
+
 
         //JSON parser stuff
 
+
         cards[0].setText(cardText[0]);
         cards[1].setText(cardText[1]);
+        cards[2].setText(cardText[2]);
+        cards[3].setText(cardText[3]);
 
         //set up on click listeners
         cards[0].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 cards[0].setBackgroundColor(Color.parseColor("#34546b"));
-                if(selectedCard != -1){
-                    cards[selectedCard].setBackgroundColor(Color.parseColor("#ffffff"));
+                if(selectedCard != -1 && selectedCard != 0){
+                    cards[selectedCard].setBackgroundColor(Color.parseColor("#fafafa"));
                 }
                 selectedCard = 0;
             }
@@ -65,10 +123,40 @@ public class PlayerActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 cards[1].setBackgroundColor(Color.parseColor("#34546b"));
-                if(selectedCard != -1){
-                    cards[selectedCard].setBackgroundColor(Color.parseColor("#ffffff"));
+                if(selectedCard != -1 && selectedCard != 1){
+                    cards[selectedCard].setBackgroundColor(Color.parseColor("#fafafa"));
                 }
                 selectedCard = 1;
+            }
+        });
+
+        cards[2].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cards[2].setBackgroundColor(Color.parseColor("#34546b"));
+                if(selectedCard != -1 && selectedCard != 2){
+                    cards[selectedCard].setBackgroundColor(Color.parseColor("#fafafa"));
+                }
+                selectedCard = 2;
+            }
+        });
+
+        cards[3].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cards[3].setBackgroundColor(Color.parseColor("#34546b"));
+                if(selectedCard != -1 && selectedCard != 3){
+                    cards[selectedCard].setBackgroundColor(Color.parseColor("#fafafa"));
+                }
+                selectedCard = 3;
+            }
+        });
+
+        update = (Button) findViewById(R.id.reset);
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                update();
             }
         });
     }
