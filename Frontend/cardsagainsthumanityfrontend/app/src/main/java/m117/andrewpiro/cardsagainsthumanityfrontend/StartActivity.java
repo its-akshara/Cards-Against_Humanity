14be8011d20bba13f1641b7525a2ef4a139ef672
its-akshara <akshara.sundar@gmail.com>
2018-05-15 14:12:17 -0700

    Firebase code adding

--- Frontend/cardsagainsthumanityfrontend/app/src/main/java/m117/andrewpiro/cardsagainsthumanityfrontend/StartActivity.java
+++ Frontend/cardsagainsthumanityfrontend/app/src/main/java/m117/andrewpiro/cardsagainsthumanityfrontend/StartActivity.java
@@ -6,9 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class StartActivity extends AppCompatActivity {
     Button startButton;
+
+
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+       // Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+       // sendRegistrationToServer(refreshedToken);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
