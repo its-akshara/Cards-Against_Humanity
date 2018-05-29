35444ece47498b5344e46fb5e45634eea8e98214
Akshara <akshara.sundar@gmail.com>
2018-05-29 13:17:38 -0700

    Setting up backend branch

--- Frontend/cardsagainsthumanityfrontend/app/src/main/java/m117/andrewpiro/cardsagainsthumanityfrontend/StartActivity.java
+++ Frontend/cardsagainsthumanityfrontend/app/src/main/java/m117/andrewpiro/cardsagainsthumanityfrontend/StartActivity.java
@@ -15,12 +15,7 @@ public class StartActivity extends AppCompatActivity {
     public void onTokenRefresh() {
         // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-       // Log.d(TAG, "Refreshed token: " + refreshedToken);
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
-       // sendRegistrationToServer(refreshedToken);
+       
     }
 
     @Override
